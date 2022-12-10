26d83c08de0d50ca0f3a3af92f81a8193014864c
Kuyho Chung <kchung1995@gmail.com>
2022-12-10 23:01:41 +0900

    프로젝트 생성

---
+++ src/main/java/jpabook/inflearnspringactual/Hello.java
@@ -0,0 +1,9 @@
+package jpabook.inflearnspringactual;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class Hello {
+    private String data;
+}
---
+++ src/main/java/jpabook/inflearnspringactual/InflearnSpringActualApplication.java
@@ -0,0 +1,13 @@
+package jpabook.inflearnspringactual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflearnSpringActualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InflearnSpringActualApplication.class, args);
+	}
+
+}
